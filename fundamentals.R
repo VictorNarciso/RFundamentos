ac865aa7b8d5120f2bf72b839dbc15f10c4ab80d
VictorNarciso <victor.tecnologiageral@hotmail.com>
2021-03-06 09:25:01 -0300

    "Exemplo de operações entre vetores e comparação"

--- fundamentals.R
+++ fundamentals.R
@@ -21,3 +21,23 @@ number1 + number2
 temps <- c(72, 71, 68, 73, 75, 76, 79)
 names(temps) <- c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
 temps
+
+
+# Vector Operations
+
+v1 <- c(5,7,9)
+v2 <- c(8,6,3)
+v1 + v2
+v1 / v2
+v1 * v2
+
+# Comparison Operators
+
+v3 <- c(5,7,9,11)
+v3 < 0
+v3 > 11
+v3 > 5
+
+v4 <- c(55,69,93,114)
+v3 + v4
+v4 - v3
